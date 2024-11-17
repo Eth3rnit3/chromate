fc75d3242b25fd2705dd4d59a9af5fba5983ce55
Eth3rnit3 <eth3rnit3@gmail.com>
2024-11-17 15:20:58 +0100

    use dev servers

--- docker_root/app.rb
+++ docker_root/app.rb
@@ -1,9 +1,24 @@
 require 'bundler/setup'
 require 'chromate'
+require '/chromate/spec/support/server'
 
-browser = Chromate::Browser.new(headless: false, xfvb: true, record: true, native_control: false)
-browser.start
-browser.navigate_to('https://www.google.com')
-sleep 5
-browser.screenshot('google.png')
-browser.stop
+class TestInDocker
+  include Support::Server
+
+  def run
+    start_servers
+    browser = Chromate::Browser.new(headless: false, xfvb: true, record: true, native_control: false)
+    browser.start
+    browser.screenshot('dom_actions_1.png')
+    browser.navigate_to(server_urls['dom_actions'])
+    browser.screenshot('dom_actions_2.png')
+    browser.refresh
+    browser.screenshot('dom_actions_3.png')
+    sleep 5
+    browser.screenshot('dom_actions_4.png')
+    browser.stop
+    stop_servers
+  end
+end
+
+TestInDocker.new.run
