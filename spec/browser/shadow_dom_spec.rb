8115176d2d21cde7876c3aa57b57813b3c429d7e
Eth3rnit3 <eth3rnit3@gmail.com>
2024-11-29 21:55:49 +0100

    add complex login

--- spec/browser/shadow_dom_spec.rb
+++ spec/browser/shadow_dom_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe 'Shadow dom' do
     browser.start
     url = server_urls['shadow_checkbox']
     browser.navigate_to(url)
-    browser.refresh
     shadow_container = browser.find_element('#shadow-container')
     expect(shadow_container).to be_shadow_root
     checkbox = shadow_container.find_shadow_child('#shadow-checkbox')
@@ -26,18 +25,31 @@ RSpec.describe 'Shadow dom' do
     browser.start
     url = server_urls['complex_login']
     browser.navigate_to(url)
-    browser.refresh
+    # Find and click the secure login container
+    secure_login = browser.find_element('secure-login')
+    locked_overlay = secure_login.find_shadow_child('#locked-overlay')
+    locked_overlay.click
 
-    browser.find_element('#locked-overlay').click
+    # Handle the security challenge
+    challenge_code = secure_login.find_shadow_child('#challenge-code').text
+    challenge_input = secure_login.find_shadow_child('#challenge-input')
+    challenge_input.type(challenge_code)
+    verify_button = secure_login.find_shadow_child('#verify-challenge')
+    verify_button.click
 
-    challenge_code = browser.find_element('#challenge-code').text
-    browser.find_element('#challenge-input').type(challenge_code)
-    browser.find_element('#verify-challenge').click
+    # Fill in the login form
+    username_input = secure_login.find_shadow_child('#username')
+    password_input = secure_login.find_shadow_child('#password')
+    username_input.type('admin')
+    password_input.type('password')
 
-    browser.find_element('#username').type('admin')
-    browser.find_element('#password').type('password')
-    browser.find_element('button[type="submit"]').click
+    # Submit the login form
+    login_form = secure_login.find_shadow_child('#login-form')
+    login_form.find_element('button').click
+    secure_zone = secure_login.find_shadow_child('#secure-zone')
+    expect(secure_zone.text).to include('Accès autorisé')
 
+    # Take a screenshot of the secure zone
     browser.screenshot('spec/apps/complex_login/secure_zone.png')
 
     browser.stop
